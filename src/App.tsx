f8d2c0d5b1c59e00fa28f6f1cfaa89a2f900a10d
emilybonar <emilykbonar@gmail.com>
2021-10-19 17:10:22 -0500

    Updated import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import Slider from "@mui/material/slider";
+import { Slider } from "@mui/material";
 import "./App.css";
 
 function App() {
