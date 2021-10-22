5d1336291b9918cb6dfe8f1bf5d375d779c40a81
emilybonar <emilykbonar@gmail.com>
2021-10-22 11:13:02 -0500

    Add infobox tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -87,6 +87,8 @@ function App() {
 						hue={hue}
 						saturation={saturation}
 						lightness={lightness}
+						numStripes={steps}
+						index={idx}
 					/>
 				))}
 			</main>
@@ -98,22 +100,81 @@ interface StripeProps {
 	hue: number;
 	saturation: number;
 	lightness: number;
+	numStripes: number;
+	index: number;
 }
 
 const Stripe: React.FC<StripeProps> = ({
 	hue,
 	saturation,
 	lightness,
+	numStripes,
+	index,
 }: StripeProps) => {
+	const hsl = [hue / 360, saturation / 100, lightness / 100];
+	const rgb = hslToRgb(hsl[0], hsl[1], hsl[2]);
 	return (
-		<div
-			style={{
-				height: "100%",
-				flexGrow: 1,
-				backgroundColor: `hsl(${hue}deg, ${saturation}%, ${lightness}%)`,
-			}}
-		/>
+		<>
+			<div
+				className="stripe"
+				style={{
+					height: "100%",
+					display: "flex",
+					flexDirection: "column",
+					maxWidth: `${100 / numStripes}%`,
+					backgroundColor: `hsl(${hue}deg, ${saturation}%, ${lightness}%)`,
+				}}
+			>
+				<div
+					className="tooltip"
+					style={{
+						backgroundColor: "white",
+						left: index === 0 ? "50%" : index === numStripes - 1 ? "-50%" : 0,
+					}}
+				>
+					<h2>HSL</h2>
+					<p>
+						{Math.round(hue)}, {Math.round(saturation)}, {Math.round(lightness)}
+					</p>
+					<h2>RGB</h2>
+					<p>
+						{rgb[0]}, {rgb[1]}, {rgb[2]}
+					</p>
+				</div>
+			</div>
+		</>
 	);
 };
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ */
+function hslToRgb(h: number, s: number, l: number) {
+	let r, g, b;
+
+	if (s == 0) {
+		r = g = b = l; // achromatic
+	} else {
+		let hue2rgb = function hue2rgb(p, q, t) {
+			if (t < 0) t += 1;
+			if (t > 1) t -= 1;
+			if (t < 1 / 6) return p + (q - p) * 6 * t;
+			if (t < 1 / 2) return q;
+			if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+			return p;
+		};
+
+		let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		let p = 2 * l - q;
+		r = hue2rgb(p, q, h + 1 / 3);
+		g = hue2rgb(p, q, h);
+		b = hue2rgb(p, q, h - 1 / 3);
+	}
+
+	return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
 
 export default App;
